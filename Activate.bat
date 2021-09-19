bd45b8565a552f4025a6c0c530418aad6e4689d9
오현석 <hyunsuk9956@naver.com>
2021-09-19 23:55:39 +0800

    config 변경

--- Activate.bat
+++ Activate.bat
@@ -1,4 +1,5 @@
 call ..\..\Scripts\activate.bat
 set FLASK_APP=pybo
 set FLASK_ENV=development
+set APP_CONFIG_FILE=C:\Users\hyuns\OneDrive\πŸ≈¡ »≠∏È\Programming2\flask\projects\myproject\config\development.py
 cmd/k
